b4551e1a547e2c4ecc64d3ce6001201ed1427407
Rossy167 <37879439+Rossy167@users.noreply.github.com>
2020-04-27 15:33:57 +0100

    Build.ps1 fleshed out
    Updated the readme too

--- README.md
+++ README.md
@@ -1,6 +1,50 @@
 # windows10-setup-script
-A few little scripts and files for when I build a new Win10 PC
 
-## 
-This a big work in progress. I intend more on the build.ps1 part being supplemental, and the real meat happening in a custom .iso and am working on getting learning how to do that
-The appearance is also a big thing. Which is going to require me getting waaaaay better at them PS Regedits.
+## Introduction
+I personally have 2 use cases Windows 10:
+    1. Gaming
+    2. Active Directory/ general Microsofty Sys Admin work
+
+Since I'm unlikely to be doing AD work related things on a personal PC, the only thing I really want from my Windows 10 home PC is for it to be fast and clean. For me a gaming PC needs to be as simple as possible, it can be versatile and have a lot of uses, but these machines get clogged up fast, and Microsoft isn't helping by giving me a clogged up Windows 10 instance by default.
+
+## A Play by Play
+
+I figured it would be mostly useless to package my own custom .iso of Windows, since it's a 5GB file and for the most part the custom package removal doesn't really help a huge amount.
+
+The core of this script is in the build.ps1 file.
+
+The script starts by staging some updates, it doesn't really properly update, but I figured that we may as well do it while we are running Powershell as admin. It then runs some scripts that I have procurred and modified from around the internet. These scripts are located in the .\scripts\ directory.
+
+    1. DebloaterWin10.ps1 - a fairly well known script at https://github.com/Sycnex/Windows10Debloater that uses Powershell and Registry edits to remove all sorts of bloat Win10 comes with
+    2. disableconsumerfeatures.reg - a basic reg edit to disable Candy Crush, Solitaire, and other non-business applications from popping up on the Start Menu
+    3. uninstall_onedrive.bat - uninstalls onedrive :O which is shockingly hard to do thonks Bill
+
+After these scripts have ran we do some regediting to remove startup items. I want nothing on startup with this device. Which I know is a pipedream, but you know, close. We then disable indexing on all drives, which is one of those random things which doesn't really speed up the device but does wear and tear the HDDs for no reason. 
+
+Taskbar.bat just removes the crappy search bar that doesn't do anything in the bottom left hand corner, making for a cleaner aesthetic. We edit the explorer settings so that we can view file extensions by default. Next :clap: turning :clap: dark :clap: mode :clap: on. Which is very important. Obviously. 
+
+Game time started. Now we install choco and get it installing all the basics I want on a fresh windows installation. Which will change from time to time, but basically: a web browser, utilities, messaging and steam.
+
+After that we're pretty much done. Just clean up the desktop from all the installations. Set Java to do it's thing since it doesn't do that itself for some reason, thanks Minecraft for forcing me to acknowledge Java exists. A teeny bit more cleanup, stopping Windows from running nonsense. Set a quick bit of policy silently in OandO.
+
+Wait for everything to stop processing, and perform a restart to clean up the stragglers.
+
+## TL;DR
+
+On a fresh windows 10 build you can extract the contents of this repo and then run build.ps1 it makes things cleaner
+
+To run it: 
+* Press the Windows key
+* Type "powershell"
+* Right click on "powershell" and select "run as administrator"
+* Paste the following: `Set-ExecutionPolicy unrestricted`
+* Run `build.ps1`
+
+## Download Windows 10
+Use the [Windows media creation tool](https://www.microsoft.com/en-ca/software-download/windows10) to get a copy of Windows 10.
+
+## O&O Shut Up 10
+I used [O&O Shut Up 10](https://www.oo-software.com/en/shutup10) using the `ooshutup10.cfg` file as a parameter. 
+
+## Resources Used (will add to later)
+* https://github.com/Sycnex/Windows10Debloater
---
+++ build.ps1
@@ -0,0 +1,109 @@
+# run this:
+    # Set-Location $wherever-you-downloaded-the-setup-script
+    # Set-ExecutionPolicy Unrestricted -Scope CurrentUser
+    # .\build.ps1
+
+
+# pay lip service to the idea of updates while we're at it
+
+Install-PackageProvider NuGet 
+Install-Module PSWindowsUpdate 
+Get-WindowsUpdate -ForceDownload
+Get-WindowsUpdate -ForceInstall
+
+# debloat 
+
+.\scripts\DebloatWin10.ps1
+.\scripts\disableconsumerfeatures.reg
+.\scripts\uninstall_onedrive.bat
+
+# remove start up items
+
+$properties = Get-Item -path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run | Select-Object -ExpandProperty property
+$properties | ForEach-Object { Remove-ItemProperty -path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run -name $_ }
+
+$properties = Get-Item -path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run | Select-Object -ExpandProperty property
+$properties | ForEach-Object { Remove-ItemProperty -path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run -name $_ }
+
+function Disable-Indexing {
+    Param($Drive)
+    $obj = Get-WmiObject -Class Win32_Volume -Filter "DriveLetter='$Drive'"
+    $indexing = $obj.IndexingEnabled
+    if("$indexing" -eq $True){
+        write-host "Disabling indexing of drive $Drive"
+        $obj | Set-WmiInstance -Arguments @{IndexingEnabled=$False} | Out-Null
+    }
+}
+
+$drives = get-volume | Select-Object -ExpandProperty driveletter
+$drives | ForEach-Object { Disable-Indexing $_":" }
+
+# appearance
+$key = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'
+Set-ItemProperty $key Hidden 0
+Set-ItemProperty $key HideFileExt 0
+Set-ItemProperty $key ShowSuperHidden 1
+New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0
+.\scripts\taskbar.bat
+
+# install all wanted packages (basically rebloat ngl)
+
+.\scripts\InstallChoco.ps1
+
+$packages = @('sysinternals', 'chromium', 'github-desktop', 'firefox', 'steam', 'vscode', 'javaruntime', 'jdk11', 'vlc', '7zip', 'qbittorrent', 'python', 'discord', 'notepad++')
+$packages | ForEach-Object {choco install $_ -y}
+Write-Host 'Giving everything time to install'
+Start-Sleep -Seconds 60
+
+# remove all the shit that choco has dumped on the desktop
+
+Remove-Item C:\Users\*\Desktop\*lnk �Force
+
+# Set the java environment variables because it beats doing it manually
+
+$path = 'C:\Program Files\Java'
+$jdk = Get-ChildItem -Path $path -Filter "jdk*" | Select-Object -ExpandProperty FullName
+$jre = Get-ChildItem -Path $path -Filter "jre*" | Select-Object -ExpandProperty FullName
+
+setx /M JAVA_HOME $jdk
+setx /M JRE_HOME $jre
+
+# disable scheduled tasks
+      
+schtasks /Change /TN "Microsoft\Windows\Application Experience\ProgramDataUpdater" /Disable | out-null
+schtasks /Change /TN "Microsoft\Windows\AppID\SmartScreenSpecific" /Disable | out-null
+schtasks /Change /TN "Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser" /Disable | out-null
+schtasks /Change /TN "Microsoft\Windows\Customer Experience Improvement Program\Consolidator" /Disable | out-null
+schtasks /Change /TN "Microsoft\Windows\Customer Experience Improvement Program\KernelCeipTask" /Disable | out-null
+schtasks /Change /TN "Microsoft\Windows\Customer Experience Improvement Program\UsbCeip" /Disable | out-null
+schtasks /Change /TN "Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticDataCollector" /Disable | out-null
+schtasks /Change /TN "Microsoft\Windows\NetTrace\GatherNetworkInfo" /Disable | out-null
+schtasks /Change /TN "Microsoft\Windows\Windows Error Reporting\QueueReporting" /Disable | out-null
+
+# disable services
+
+cmd /c sc config DiagTrack start= disabled | out-null
+cmd /c sc config dmwappushservice start= disabled | out-null
+cmd /c sc config diagnosticshub.standardcollector.service start= disabled | out-null
+cmd /c sc config TrkWks start= disabled | out-null
+cmd /c sc config WMPNetworkSvc start= disabled | out-null # Shouldn't exist but just making sure ...
+Set-Content C:\ProgramData\Microsoft\Diagnosis\ETLLogs\AutoLogger\AutoLogger-Diagtrack-Listener.etl -Value "" -Force
+
+# mess with the screwey windows update and delivery settings
+
+New-ItemProperty -ErrorAction SilentlyContinue -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeliveryOptimization\Config" -Name "DownloadMode" -PropertyType DWORD -Value 0 | Out-Null
+Set-ItemProperty -ErrorAction SilentlyContinue -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeliveryOptimization\Config" -Name "DODownloadMode" -Value 0 | Out-Null
+Set-ItemProperty -ErrorAction SilentlyContinue -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeliveryOptimization\" -Name "SystemSettingsDownloadMode" -Value 0 | Out-Null
+Set-ItemProperty -ErrorAction SilentlyContinue -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager\" -Name "SystemPaneSuggestionsEnabled" -Value 0 | Out-Null
+
+# o and o stuff
+$path = Get-Location
+$path = $path.path + "\OOSU10.exe"
+Invoke-WebRequest -Uri "https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe" -OutFile $path
+Start-Sleep -Seconds 15
+.\OOSU10.exe ooshutup10.cfg /silent /nosrp
+
+#Restart PC
+Write-Host 'Restarting in 30 seconds'
+Start-Sleep -Seconds 30
+Restart-Computer -Force
