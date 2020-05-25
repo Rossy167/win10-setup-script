0f25ffef879546453e49806a359a40b8402c2912
Rossy167 <37879439+Rossy167@users.noreply.github.com>
2020-05-25 13:23:02 +0100

    Added steam blocker functions to profile

--- build.ps1
+++ build.ps1
@@ -126,6 +126,19 @@ Set-Content -Path $profile -Value "function Stop-AMDBloat {
 
 function fish {
     bash -c 'fish'
+}
+
+function Block-Steam {
+        New-NetFirewallRule -Action block -Program 'C:\Program Files (x86)\Common Files\Steam\SteamService.exe' -Profile any -Direction Outbound -Displayname 'Block-Steam Rossy' | Out-Null
+        New-NetFirewallRule -Action block -Program 'C:\Program Files (x86)\Steam\bin\cef\cef.win7x64\steamwebhelper.exe' -Profile any -Direction Outbound -Displayname 'Block-Steam Rossy' | Out-Null
+        New-NetFirewallRule -Action block -Program 'C:\program files (x86)\steam\steam.exe' -Profile any -direction Outbound -Displayname 'Block-Steam Rossy' | Out-Null
+        New-NetFirewallRule -Action block -Program 'C:\Program Files (x86)\Common Files\Steam\SteamService.exe' -Profile any -Direction Inbound -Displayname 'Block-Steam Rossy' | Out-Null
+        New-NetFirewallRule -Action block -Program 'C:\Program Files (x86)\Steam\bin\cef\cef.win7x64\steamwebhelper.exe' -Profile any -Direction Inbound -Displayname 'Block-Steam Rossy' | Out-Null
+        New-NetFirewallRule -Action block -Program 'C:\program files (x86)\steam\steam.exe' -Profile any -direction Inbound -Displayname 'Block-Steam Rossy' | Out-Null
+}
+
+function Unblock-Steam {
+        Get-NetFirewallRule | Where-Object DisplayName -eq 'Block-Steam Rossy' | Remove-NetFirewallRule | Out-Null
 }" -Force
 
 # enable bash
